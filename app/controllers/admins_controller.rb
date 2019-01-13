3484197cd4be56b3b6d38f8ad41096cf06717ef5
unknown <i1i1.00263@gmail.com>
2019-01-13 16:35:32 +0900

    add controller2

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -1,4 +1,19 @@
 class AdminsController < ApplicationController
+  def edit
+  end
+
+  def update
+  end
+
   def show
+  end
+
+  def user_history
+  end
+
+  def user_index
+  end
+
+  def user_search
   end
 end
--- app/controllers/cart_items_controller.rb
+++ app/controllers/cart_items_controller.rb
@@ -1,2 +1,13 @@
 class CartItemsController < ApplicationController
+  def add_cart_item
+  end
+
+  def cart_index
+  end
+
+  def update_volume
+  end
+
+  def destroy_cart_item
+  end
 end
