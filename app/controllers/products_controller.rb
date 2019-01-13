1f6f7c985aa87fd0e3f0933cab24d8c57d91c127
Your Name <you@example.com>
2019-01-13 20:18:24 +0900

    update products#index

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,12 +1,16 @@
 class ProductsController < ApplicationController
   def new
-    @post = Product.new
+    @product = Product.new
   end
 
   def create
+    product = Product.new(product_params)
+    product.save
+    redirect_to products_path
   end
 
   def index
+    @products = Product.all
   end
 
   def show
@@ -20,4 +24,12 @@ class ProductsController < ApplicationController
 
   def destroy
   end
+
+  private
+    def product_params
+      params.require(:product).permit(:name, :price, :image)
+    end
+
 end
+
+
