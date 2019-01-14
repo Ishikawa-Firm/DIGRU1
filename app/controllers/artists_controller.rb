bf6214b4334727947173d6b3521fd10c509c1f84
石川悠生 <ishikawayuki@YukinoMacBook-Pro.local>
2019-01-14 19:25:24 +0900

    artist view update

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,36 +1,29 @@
 class ArtistsController < ApplicationController
   def index
-  # ビューあり
   @artists = Artist.all
   end
 
   def show
-  # ビューあり
   @artist = Artist.find(params[:id])
   end
 
   def edit
-  # ビューあり
   @artist = Artist.find(params[:id])
   end
 
   def update
-  # ビューなし
   @artist = Artist.find(params[:id])
   @artist.update(artist_params)
   redirect_to artist_path(@artist.id)
   end
 
   def destroy
-  # ビューなし
   end
 
   def search
-  # ビューあり
   end
 
   def history
-  # ビューあり
   end
 
   def artist_params
