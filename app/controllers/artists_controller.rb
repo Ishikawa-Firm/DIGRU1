8d1c23e63b0ee5bd82c955651eb11acdb695faf5
Yuki-Ishikawa <youki.ishi.work@gmail.com>
2019-01-15 05:31:44 +0000

    artist controller view update

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,31 +1,33 @@
 class ArtistsController < ApplicationController
   def index
-  # ビューあり
-  @artists = Artist.all
+    @artists = Artist.all
   end
 
   def show
-  # ビューあり
-  @artist = Artist.find(params[:id])
+    @artist = Artist.find(params[:id])
   end
 
   def edit
-  # ビューあり
+    @artist = Artist.find(params[:id])
   end
 
   def update
-  # ビューなし
+    @artist = Artist.find(params[:id])
+    @artist.update(artist_params)
+    redirect_to artist_path(@artist.id)
   end
 
   def destroy
-  # ビューなし
   end
 
   def search
-  # ビューあり
   end
 
   def history
-  # ビューあり
   end
+
+  def artist_params
+    params.require(:artist).permit(:email)
+  end
+
 end
