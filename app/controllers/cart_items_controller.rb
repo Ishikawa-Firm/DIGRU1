118ab4175bd76b2d6d976dcca0227fb9b6deab75
Your Name <you@example.com>
2019-01-15 16:30:13 +0900

    delete OrderDetails and Orders, add Carts

--- app/controllers/cart_items_controller.rb
+++ app/controllers/cart_items_controller.rb
@@ -1,17 +1,2 @@
 class CartItemsController < ApplicationController
-  def add_cart_item
-  	session[:cart_item] = [] unless session[:cart_item]
-    session[:cart_item] << params[:product_id]
-    flash[:success] = "商品がカートに追加されました．"
-    redirect_to cart_items_path
-  end
-
-  def cart_index
-  	@cart = session[:cart_item].map { |product_id| CartItem.find(product_id) }
-  end
-
-  def update_volume
-  end
-
-  def destroy_cart_item
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
     has_many :addresses
-    has_many :cart_items
+    has_many :orders
 
   def self.search(search)
     if search
