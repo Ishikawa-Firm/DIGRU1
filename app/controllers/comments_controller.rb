c484e272dbef3a771f1d301e306b609d665c9e8a
unknown <i1i1.00263@gmail.com>
2019-01-20 14:08:46 +0900

    add comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,7 +2,7 @@ class CommentsController < ApplicationController
   def create
     @product = Product.find(params[:product_id])
     @comment = current_user.comments.new(comment_params)
-    @comment.product_id = product.id
+    @comment.product_id = @product.id
     @comment.save
     redirect_to products_path(@product)
   end
@@ -15,6 +15,9 @@ class CommentsController < ApplicationController
 
   private
   def comment_params
-  params.require(:comment).permit(:user_id, :product_id, :comment_text)
+  params.require(:comment).permit(:user_id,
+                                  :product_id, :comment_text)
   end
 end
+
+
