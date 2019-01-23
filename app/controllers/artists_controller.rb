7be85d6fe7179e89ac8fcc1e60ebef9337eab542
Your Name <you@example.com>
2019-01-23 23:55:44 +0900

    update artists product_history enum

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -33,7 +33,9 @@ class ArtistsController < ApplicationController
     @products = current_artist.products.all
     @carts = Cart.where(user_id: User.all)
     @histories = CartItem.where(product_id: @products, cart_id: @carts)
-
+    @histories.each do |h|
+      @user = h.cart.user.addresses.find_by(["id = ?", h.cart.address_id])
+    end
   end
 
   private
