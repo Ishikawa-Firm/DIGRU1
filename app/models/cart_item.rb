f5f2267cecaccdeb01c8639224a34780d0b6b154
Your Name <you@example.com>
2019-01-24 13:46:29 +0900

    update product_history

--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -4,7 +4,7 @@ class CartItem < ApplicationRecord
 
 	enum status:{受付: 0, 商品準備中:1, 出荷済み:2, 商品到着:3}
 
-	def self.restrict_status
+	def self.restrict_status_artist
 	    statuses.except(:商品到着)
 	end
 
