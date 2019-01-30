39e1a294436812c37d88ac206a0e0c064e0d7d07
unknown <i1i1.00263@gmail.com>
2019-01-30 15:58:36 +0900

    add validation

--- app/models/disc_number.rb
+++ app/models/disc_number.rb
@@ -2,4 +2,6 @@ class DiscNumber < ApplicationRecord
 	belongs_to :product, inverse_of: :disc_numbers, optional: true
 	has_many :musics
 	accepts_nested_attributes_for :musics, allow_destroy: true
+
+	validates :disc_name, presence: true, length: { minimum: 2, maximum: 50 }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -25,5 +25,12 @@ class User < ApplicationRecord
           User.all
       end
     end
-end
 
+  validates :name, presence: true, length: { minimum: 2, maximum: 30 }
+  validates :name_kana, presence: true, length: { minimum: 2, maximum: 30 }
+  validates :hundle_name, presence: true, length: { minimum: 2, maximum: 30 }
+  validates :postal_code, presence: true, numericality: true, length: { is: 8 }
+  validates :phone_number, presence: true, length: { maximum: 11 }
+  validates :user_address, presence: true, length: { maximum: 100 }
+
+end
